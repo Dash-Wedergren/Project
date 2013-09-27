5e237e9221cfab93428f42297f6bd66b0de548f1
Dash <dashwedergren@gmail.com>
2013-09-27 11:32:28 -0500

    stuff

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-function Main() {
- alert("hi");   
+function Tracker() {
+ var random = Math.floor(Math.random()*num*2*1000000000000072*Math.random());
+ var uid = CryptoJS.MD5(random);
+ console.log(uid);
+ var date = newDate();
+ console.log(date); 
 }
